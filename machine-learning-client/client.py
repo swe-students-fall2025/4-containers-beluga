d8db70df04e8060791d2089a35cc6f68930609c8
SnazzyBeatle115 <snazzybeatle115@gmail.com>
2025-11-17 23:35:02 -0500

    the docker works

--- machine-learning-client/client.py
+++ machine-learning-client/client.py
@@ -99,4 +99,4 @@ def create_app():
 if __name__ == "__main__":
     flask_app = create_app()
     port = int(os.environ.get("PORT", 80))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    flask_app.run(host="0.0.0.0", port=port, debug=True)
